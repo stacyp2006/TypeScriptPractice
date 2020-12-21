3390e102d8a9f971202ad757c6bb8e57082215c5
Stacy Potten <stacylynnaz@gmail.com>
2020-12-21 13:18:27 -0700

    Practice with annotations for arrays, objects and classes

--- Features/annotations/variables.ts
+++ Features/annotations/variables.ts
@@ -11,3 +11,20 @@ let nothing: undefined = undefined;
 
 // built in objects
 let now: Date = new Date();
+
+//Array
+let colors: string[] = ['red', 'green', 'blue'];
+//indicates an array of strings
+
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+
+//Classes
+class Car {}
+let car: Car = new Car();
+
+//Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
