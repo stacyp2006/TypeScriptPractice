7b7bfb2e580a62ab139cda60ef5bf19daaa3bfb3
Stacy Potten <stacylynnaz@gmail.com>
2020-12-21 13:11:50 -0700

    Practice with variable notations

---
+++ Features/annotations/variables.ts
@@ -0,0 +1,13 @@
+let apples: number = 5;
+//colon and number are the annotation
+
+let speed: string = 'fast';
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+//value is same as type name
+
+let nothing: undefined = undefined;
+
+// built in objects
+let now: Date = new Date();
