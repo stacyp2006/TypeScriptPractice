8405e71ce836a603ce5a94ba624c85c7600ac1de
Stacy Potten <stacylynnaz@gmail.com>
2020-12-22 16:09:40 -0700

    fix bugs

--- arrays.ts
+++ arrays.ts
@@ -2,26 +2,22 @@ const carMakers = ['ford', 'toyota', 'chevy'];
 
 // add explicit typing when assigning variable to an empty array
 
-const dates = [new Date(), new Date();
+const dates = [new Date(), new Date()];
 
 //string[][]
-const carsByMake = [
-  ['f150'],
-  ['corolla'],
-  ['camaro']
-];
+const carsByMake = [['f150'], ['corolla'], ['camaro']];
 
 //Benefit: Help w/inference when extracting values
 const car1 = carMakers[0];
-const myCar =carMakers.pop();
+const myCar = carMakers.pop();
 
 // Benefit: prevent incompatible values
-carMakers.push(100);
+// carMakers.push(100);
 
 //Benefit: help with 'map' and other array methods like auto complete
 carMakers.map((car: string): string => {
-  return car; 
-})
+  return car;
+});
 
 //Flexible types (string | Date)[]
-const importantDates = [new Date(), '2020-12-23']; 
+const importantDates = [new Date(), '2020-12-23'];
