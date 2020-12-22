7a44f14aa5332a7ab5c7cc33fdd56c537cb0262c
Stacy Potten <stacylynnaz@gmail.com>
2020-12-22 14:42:32 -0700

    Typing with void and never

--- Features/annotations/functions.ts
+++ Features/annotations/functions.ts
@@ -13,3 +13,13 @@ function divide(a: number, b: number): number {
 const multiply = function (a: number, b: number): number {
   return a * b;
 };
+
+// Void means nothing is returned from this function, tho technically it can return null or undefined
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+// Only use it when you expect a function to never return anything ever. Never ever.
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
