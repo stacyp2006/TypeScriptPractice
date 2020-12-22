17d450a8d06f6575dad9825dc089f8777982fe5b
Stacy Potten <stacylynnaz@gmail.com>
2020-12-22 16:11:43 -0700

    Additional Tuple practice

--- tuples.ts
+++ tuples.ts
@@ -10,3 +10,10 @@ const sprite: Drink = ['clear', true, 40];
 
 //tuple type annotation
 const pepsi: [string, boolean, number] = ['brown', true, 40];
+
+const carSpecs: [number, number] = [400, 3354];
+
+const carStats = {
+  horsepower: 400,
+  weight: 3354,
+};
