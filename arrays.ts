eb89007bae9101dbd4a2307d8712f530cbfb3a74
Stacy Potten <stacylynnaz@gmail.com>
2020-12-22 15:42:10 -0700

    Typed array practice

---
+++ arrays.ts
@@ -0,0 +1,13 @@
+const carMakers = ['ford', 'toyota', 'chevy'];
+
+// add explicit typing when assigning variable to an empty array
+
+const dates = [new Date(), new Date();
+
+const carsByMake = [
+  ['f150'],
+  ['corolla'],
+  ['camaro']
+];
+
+//string[][]
