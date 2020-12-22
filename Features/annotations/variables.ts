9f79f9df9a5605ad8d2da86278f3b06f356194e3
Stacy Potten <stacylynnaz@gmail.com>
2020-12-22 14:20:45 -0700

    Practice with delayed intialization

--- Features/annotations/variables.ts
+++ Features/annotations/variables.ts
@@ -39,3 +39,13 @@ const logNumber: (i: number) => void = (i: number) => {
 const json = '{"x": 10, "y": 20}';
 const coordinates: { x: number; y: number } = JSON.parse(json);
 console.log(coordinates); //{x: 10, y: 20}
+
+// 2) When we declare a variable on one line and intialize it later
+
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean; // generally better to set this to false from the start
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
