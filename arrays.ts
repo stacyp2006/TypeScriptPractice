b2c7a4cc7cca929f23194c22bd5cf1d10216e2b9
Stacy Potten <stacylynnaz@gmail.com>
2020-12-22 15:50:38 -0700

    Practice with benefits of typed arrays

--- arrays.ts
+++ arrays.ts
@@ -4,10 +4,22 @@ const carMakers = ['ford', 'toyota', 'chevy'];
 
 const dates = [new Date(), new Date();
 
+//string[][]
 const carsByMake = [
   ['f150'],
   ['corolla'],
   ['camaro']
 ];
 
-//string[][]
+//Benefit: Help w/inference when extracting values
+const car1 = carMakers[0];
+const myCar =carMakers.pop();
+
+// Benefit: prevent incompatible values
+carMakers.push(100);
+
+//Benefit: help with 'map' and other array methods like auto complete
+carMakers.map((car: string): string => {
+  return car; 
+})
+
