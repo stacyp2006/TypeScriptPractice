1aa9fcd77c530996c95364ee7e3dbc6d57a34f43
Stacy Potten <stacylynnaz@gmail.com>
2020-12-22 14:47:11 -0700

    Destructuring annotations

--- Features/annotations/functions.ts
+++ Features/annotations/functions.ts
@@ -23,3 +23,21 @@ const logger = (message: string): void => {
 const throwError = (message: string): never => {
   throw new Error(message);
 };
+
+const forecast = {
+  date: new Date(),
+  weather: 'sunny',
+};
+
+const logWeather = ({
+  date,
+  weather,
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(forecast.date);
+  console.log(forecast.weather);
+};
+
+logWeather(forecast);
