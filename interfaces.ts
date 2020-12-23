1a8877fc5acb1c7680ab03252722ec3e47df1204
Stacy Potten <stacylynnaz@gmail.com>
2020-12-22 18:21:47 -0700

    Add drink object to demo Reportable usability

--- interfaces.ts
+++ interfaces.ts
@@ -11,10 +11,20 @@ const oldCivic = {
   },
 };
 
+const drink1 = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My drink has ${this.sugar} grams of sugar`;
+  },
+};
+
 const printSummary = (item: Reportable): void => {
   console.log(item.summary());
 };
 
 printSummary(oldCivic);
+printSummary(drink1);
 
 //TS only looks for properties in the interface, doesn't matter if you have extras
