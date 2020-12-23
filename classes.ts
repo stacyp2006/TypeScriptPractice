275339d2d0c6179f3d1683ddbe8d98ab2bad0400
Stacy Potten <stacylynnaz@gmail.com>
2020-12-22 22:55:11 -0700

    practice typing with classes and inheritance

--- classes.ts
+++ classes.ts
@@ -8,17 +8,17 @@ class Vehicle {
 const vehicle1 = new Vehicle('orange');
 console.log(vehicle1.color);
 
-// class Car1 extends Vehicle {
-//   private drive(): void {
-//     console.log('vroom');
-//   }
-//   startDrivingProcess(): void {
-//     this.drive();
-//     this.honk();
-//   }
-// }
+class Car1 extends Vehicle {
+  private drive(): void {
+    console.log('vroom');
+  }
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
+  }
+}
 
-// const car2 = new Car1();
-// car2.startDrivingProcess();
+const car2 = new Car1();
+car2.startDrivingProcess();
 
-// const vehicle = new Vehicle();
+const vehicle = new Vehicle();
