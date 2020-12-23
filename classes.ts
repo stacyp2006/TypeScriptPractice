73e38b26bac56f0a122edfbbfa760be382b5b643
Stacy Potten <stacylynnaz@gmail.com>
2020-12-22 22:36:32 -0700

    Add car class to extend vehicle

--- classes.ts
+++ classes.ts
@@ -7,6 +7,16 @@ class Vehicle {
   }
 }
 
+class Car1 extends Vehicle {
+  drive(): void {
+    console.log('vroom');
+  }
+}
+
+const car2 = new Car1();
+car2.drive();
+car2.honk();
+
 const vehicle = new Vehicle();
 
 vehicle.honk();
