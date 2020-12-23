742df1cf48aa2fb601e931146df04e62d54e257e
Stacy Potten <stacylynnaz@gmail.com>
2020-12-22 22:51:53 -0700

    Typing with fields in classes

--- classes.ts
+++ classes.ts
@@ -1,30 +1,24 @@
 class Vehicle {
-  public drive(): void {
-    console.log('chugga chugga');
-  }
+  constructor(public color: string) {}
   protected honk(): void {
     console.log('beep');
   }
 }
 
-const vehicle1 = new Vehicle();
-vehicle1.honk();
+const vehicle1 = new Vehicle('orange');
+console.log(vehicle1.color);
 
-class Car1 extends Vehicle {
-  private drive(): void {
-    console.log('vroom');
-  }
-  startDrivingProcess(): void {
-    this.drive();
-    this.honk();
-  }
-}
+// class Car1 extends Vehicle {
+//   private drive(): void {
+//     console.log('vroom');
+//   }
+//   startDrivingProcess(): void {
+//     this.drive();
+//     this.honk();
+//   }
+// }
 
-const car2 = new Car1();
-car2.startDrivingProcess();
-car2.honk();
+// const car2 = new Car1();
+// car2.startDrivingProcess();
 
-const vehicle = new Vehicle();
-
-vehicle.honk();
-vehicle.drive();
+// const vehicle = new Vehicle();
